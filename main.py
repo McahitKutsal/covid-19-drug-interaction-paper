b886a1132a525a8625d02af503e92837f425bc10
Mücahit Kutsal <64054927+McahitKutsal@users.noreply.github.com>
2022-04-03 21:21:25 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 from classes.network import MPNNModel
 from data.preprocess import MPNNDataset
-from classes.transition import graphs_from_smiles
+from classes.transition import graphs_from_smiles, molecule_from_smiles
 from custommetrics.metrics import f1
 
 from tensorflow import keras
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from rdkit.Chem.Draw import MolsToGridImage
 
 from sklearn.metrics import roc_curve
 from sklearn.metrics import auc
@@ -80,3 +81,12 @@ plt.xlabel("Epochs", fontsize=16)
 plt.ylabel("F1 Score", fontsize=16)
 plt.legend(fontsize=16)
 plt.savefig('visualizations/f1 score.png')
+
+molecules = [molecule_from_smiles(df.isosmiles.values[index])for index in test_index]
+names = [df.cmpdname.values[index] for index in test_index]
+y_true = [df.target.values[index] for index in test_index]
+y_pred = tf.squeeze(mpnn.predict(test_dataset), axis=1)
+
+legends = [f"y_true/y_pred = {y_true[i]}/{y_pred[i]:.2f} \n {names[i].split(',')[0]}" for i in range(0,8)]
+img = MolsToGridImage(molecules[0:8], molsPerRow=4, legends=legends, returnPNG=False, subImgSize=(400, 400))
+img.save("visualizations/drug results.png")
